f73f68916fa8e39a3543a047e3c25f9ee181408b
mahesh818480 <131504807+mahesh818480@users.noreply.github.com>
2025-12-10 06:48:34 +0000

    remove the unwanted code

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -9,9 +9,9 @@ import { Router } from '@angular/router';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
-  constructor(private route: Router) {}
-navigateTo(pages: string) {
-  this.route.navigate([pages]);
-  console.log('Navigating to Projects section');  
-}
+  constructor(private route: Router) { }
+  navigateTo(pages: string) {
+    this.route.navigate([pages]);
+    console.log('Navigating to Projects section');
+  }
 }
